992ddf442cd32a10aa8fe37df687fdc2fb42dd14
tychejin1218 <tychejin@naver.com>
2021-04-11 15:24:19 +0900

    싱글 파일 컴포너트 체계에서 컴포넌트 등록하기

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    {{ str }}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+// AppHeader.vue를 App.vue에서 사용하기 위해 import 
+// AppHeader.vue 파일이 AppHeader 변수명에 저장
+import AppHeader from './components/AppHeader.vue';
+
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>';
+// }
+
 // new Vue({
 //   data: {
 //     str: 'hi'
@@ -16,6 +24,9 @@ export default {
     return {
       str: 'hi'
     }
+  },
+  components: {
+    'app-header': AppHeader
   }
 }
 </script>
